3e96bf6a24d64c458a0484ed2200276392fec174
milad <seyeadmiladnouri@gmail.com>
2026-01-05 13:14:31 +0330

    metrics

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import  pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.linear_model import  LinearRegression
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 data=pd.read_csv("Advertising.csv", usecols=[ 'TV','radio','newspaper','sales'])
 df=pd.DataFrame(data)
-print(df)
+# print(df)
 
 
 
@@ -17,11 +18,23 @@ ss=StandardScaler()
 X_rescale=ss.fit_transform(X)
 
 
-X_train , X_test, Y_train , Y_test = train_test_split(X_rescale,Y , test_size=0.2 , random_state=42)
+X_train , X_test, Y_train , Y_test = train_test_split(X_rescale,Y , test_size=0.15 , random_state=42)
 
 
 model=LinearRegression()
 model.fit(X_train ,Y_train)
 y_pred = model.predict(X_test)
 
-print(y_pred)
+# print(y_pred)
+
+from sklearn import metrics
+mse  = mean_squared_error(Y_test ,y_pred)
+mae = mean_absolute_error(Y_test ,y_pred)
+rmse=np.sqrt(mse)
+r2score= r2_score(Y_test,y_pred)
+
+
+print(mse)
+print(mse)
+print(rmse)
+print(r2score)
